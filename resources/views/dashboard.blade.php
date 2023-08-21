e7247f8c0167d453155e4ef8b47e95395e42a644
Raxmatilla <56291918+Raxmatilla97@users.noreply.github.com>
2023-08-21 22:15:24 +0500

    So'ngi yangilanish

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,7 +33,7 @@
     @endif
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-[1568px] mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class=" p-6 text-gray-900 dark:text-gray-100">
 
@@ -116,7 +116,7 @@
         </div>
 
         <div class="pt-6 pb-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-[1568px] mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class=" p-6 text-gray-900 dark:text-gray-100">
                         <!-- list -->
